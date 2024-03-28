a0a914958b4592d555f87ade708df72980b2dd81
maria.akulova <maria.akulova.dragon@gmail.com>
2024-03-28 11:39:46 +0100

    feat: getAllOrNothing

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -102,8 +102,11 @@ function getFirstPromiseResult(promises) {
  * [Promise.resolve(1), Promise.resolve(2), Promise.resolve(3)] => Promise fulfilled with [1, 2, 3]
  * [Promise.resolve(1), Promise.reject(2), Promise.resolve(3)] => Promise rejected with 2
  */
-function getAllOrNothing(/* promises */) {
-  throw new Error('Not implemented');
+function getAllOrNothing(promises) {
+  const result = Promise.all(promises).then((responses) => {
+    responses.filter((response) => response.status === 'rejected');
+  });
+  return result.length < promises.length ? result[0] : result;
 }
 
 /**
